edd2fda6377df21b63a1c77f42f7d41d8d1143a9
BlackCorsairDev <104024829+BlackCorsairDev@users.noreply.github.com>
2023-02-17 04:23:13 -0500

    Update example_usage.php

--- example_usage.php
+++ example_usage.php
@@ -1,6 +1,6 @@
 <?php
 
-require(__DIR__ . '/autoload.php');  
+require(__DIR__ . '/src/autoload.php');  
 
 use Qvapay\Auth\Login;
 $auth_data = ["email" => 'blahblah@gmail.com', "password" => 'blahblahblah'];
